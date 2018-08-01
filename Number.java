4ed618731d92eb437a4cf5f11eb13837d3cdf1f8
ArunjunaiRoja <41982706+ArunjunaiRoja@users.noreply.github.com>
2018-08-01 06:08:08 +0000

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,21 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 class Number
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        if(n>0)
-        {
-            System.out.print("Positive");
-        }
-        else if(n<0)
-        {
-            System.out.print("Negative");
-        }
-        else
-        {
-            System.out.print("Zero");
-        }
-    }
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.print("Positive");
+}
+else if(n<0)
+{
+System.out.print("Negetive");
+}
+else
+{
+System.out.print("Zero");
+}
+}
 }
